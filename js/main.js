6ddde6dd3b029afd2a9d012fb5a570561a60a3b6
Лиза <86786661+lizzer69@users.noreply.github.com>
2023-02-09 12:17:47 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	slidesPerView: 2,
+	spaceBetween: 20,
+	navigation: {
+		nextEl: '.swiper-button-left',
+		prevEl: '.swiper-button-right',
+	},
+})
